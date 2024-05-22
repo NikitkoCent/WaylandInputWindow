c82b5bfb742621fe4ca49eaf9a27f9d049e06ca4
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2024-05-22 19:06:48 +0200

    Add RAII wrapper for wl_* resources.

--- utilities.h
+++ utilities.h
@@ -2,7 +2,9 @@
 #define WAYLAND_INPUT_WINDOW_UTILITIES_H
 
 #include <type_traits>      // std::remove_cv_t, std::remove_reference_t, std::is_pointer_v, std::is_same_v
-#include <utility>          // std::forward
+#include <utility>          // std::move, std::forward, std::pair
+#include <optional>         // std::optional
+#include <functional>       // std::function
 #include <string_view>      // std::string_view
 #include <chrono>           // std::chrono::*
 #include <ctime>            // std::localtime, std::strftime
@@ -12,6 +14,77 @@
 #include <sstream>          // std::ostringstream
 #include <cstdio>           // std::snprintf
 #include <cerrno>           // errno
+
+
+template<typename T>
+class WLResourceWrapper
+{
+public:
+    WLResourceWrapper() noexcept = default;
+
+    template<typename CustomDeleter>
+    WLResourceWrapper(T&& resource, CustomDeleter&& deleter)
+        : impl_{ std::in_place, std::move(resource), std::forward<CustomDeleter>(deleter) }
+    {}
+
+    explicit WLResourceWrapper(T&& resource) noexcept( std::is_nothrow_move_constructible_v<T> )
+        : WLResourceWrapper(std::move(resource), nullptr)
+    {}
+
+    WLResourceWrapper(const WLResourceWrapper&) = delete;
+    WLResourceWrapper(WLResourceWrapper&&) = default;
+
+    ~WLResourceWrapper() noexcept
+    {
+        reset();
+    }
+
+public:
+    WLResourceWrapper& operator=(const WLResourceWrapper&) = delete;
+    WLResourceWrapper& operator=(WLResourceWrapper&& rhs) noexcept( noexcept(reset()) && noexcept(impl_ = std::move(rhs.impl_)) )
+    {
+        if (&rhs != this)
+        {
+            reset();
+            impl_ = std::move(rhs.impl_);
+        }
+
+        return *this;
+    }
+
+public:
+    [[nodiscard]] bool hasResource() const noexcept { return impl_.has_value(); }
+
+    [[nodiscard]] T& getResource() { return impl_.value().first; }
+    [[nodiscard]] const T& getResource() const { return impl_.value().first; }
+
+    [[nodiscard]] T& operator*() { return getResource(); }
+    [[nodiscard]] const T& operator*() const { return getResource(); }
+
+public:
+    void reset()
+    {
+        if (impl_.has_value())
+        {
+            auto& deleter = impl_->second;
+            if (deleter)
+                deleter(impl_->first);
+            impl_.reset();
+        }
+    }
+
+private:
+    std::optional< std::pair<T, std::function<void(T&)>> > impl_;
+};
+
+template<typename T, typename TF, typename D>
+WLResourceWrapper< std::decay_t<T> > makeWLResourceWrapperChecked(T&& resource, const TF& invalid, D&& deleter)
+{
+    if (bool(std::forward<T>(resource) == invalid) == true)
+        return {};
+
+    return { std::forward<T>(resource), std::forward<D>(deleter) };
+}
 
 
 namespace logging {
